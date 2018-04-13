212175fa8007678cf993b4de90c1114cd2e964ac
mktcode <kontakt@markus-kottlaender.de>
2018-04-14 00:50:32 +0200

    hid menu items

--- components/NavbarLoggedIn.vue
+++ components/NavbarLoggedIn.vue
@@ -5,8 +5,8 @@
 
     <b-collapse is-nav id="nav_collapse">
       <b-navbar-nav class="mx-auto">
-        <b-nav-item href="#">Story Archive</b-nav-item>
-        <b-nav-item href="#">FAQ</b-nav-item>
+        <!--<b-nav-item href="#">Story Archive</b-nav-item>-->
+        <!--<b-nav-item href="#">FAQ</b-nav-item>-->
         <b-nav-item v-b-modal.userModal>{{ user.name }}</b-nav-item>
         <b-nav-item @click="logout">logout</b-nav-item>
       </b-navbar-nav>
