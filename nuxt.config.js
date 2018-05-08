40935662736d119b668623921b440297150b495b
mktcode <kontakt@markus-kottlaender.de>
2018-05-08 21:00:41 +0200

    fixed keywords

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,7 @@ module.exports = {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
-      { hid: 'keywords', name: 'keywords', content: pkg.keywords }
+      { hid: 'keywords', name: 'keywords', content: pkg.keywords.join(',') }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
