e151ab189c6d319180942e615d1296c7f5c641ef
mktcode <kontakt@markus-kottlaender.de>
2018-05-09 11:26:34 +0200

    added bottom margin to images in the story

--- components/StoryPart.vue
+++ components/StoryPart.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center mb-3">
     <div v-if="part.appendText">{{ part.appendText }}</div>
-    <div v-if="part.image">
+    <div v-if="part.image" class="mb-1">
       <img :src="part.image" alt="" class="img-fluid" />
     </div>
     <sup>by <a :href="'https://steemit.com/@' + part.author" target="_blank">@{{ part.author }}</a></sup>
