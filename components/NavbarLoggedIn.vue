1dccc61c45ed9bc31f3290947d5691968f499030
mktcode <kontakt@markus-kottlaender.de>
2018-05-26 23:40:59 +0200

    languages switcher

--- components/NavbarLoggedIn.vue
+++ components/NavbarLoggedIn.vue
@@ -5,6 +5,11 @@
 
     <b-collapse is-nav id="nav_collapse">
       <b-navbar-nav class="mx-auto">
+        <b-nav-item v-b-modal.langModal>
+          <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+            <path d="M17.9,17.39C17.64,16.59 16.89,16 16,16H15V13A1,1 0 0,0 14,12H8V10H10A1,1 0 0,0 11,9V7H13A2,2 0 0,0 15,5V4.59C17.93,5.77 20,8.64 20,12C20,14.08 19.2,15.97 17.9,17.39M11,19.93C7.05,19.44 4,16.08 4,12C4,11.38 4.08,10.78 4.21,10.21L9,15V16A2,2 0 0,0 11,18M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
+          </svg>
+        </b-nav-item>
         <li class="nav-item">
           <nuxt-link to="/" class="nav-link">
             <svg style="width:20px;height:20px" viewBox="0 0 24 24">
