5cb8d1d7f8ea2c65f4aef4a5982cdbdc0a9b2ad7
mktcode <kontakt@markus-kottlaender.de>
2018-05-29 11:22:03 +0200

    translated untranslated text, now for real!

--- components/Command.vue
+++ components/Command.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="comment text-center">
     <div class="comment-profile-image" :style="{ backgroundImage: 'url(https://steemitimages.com/u/' + command.author + '/avatar/small)' }"></div>
-    <div class="comment-username"><a :href="'https://steemit.com/@' + command.author" target="_blank">{{ command.author }}</a> wrote:</div>
+    <div class="comment-username"><a :href="'https://steemit.com/@' + command.author" target="_blank">{{ command.author }}</a> {{ $t('command.wrote') }}</div>
     <div class="comment-command" v-html="appendHtml" v-if="meta.appendText"></div>
     <div v-if="meta.image" class="text-center my-3">
       <img :src="meta.image" alt="" class="img-fluid" />
