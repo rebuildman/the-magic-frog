560989f41df88719b14dbbf39e124954ae094770
mktcode <kontakt@markus-kottlaender.de>
2018-06-10 15:49:49 +0200

    reformatted code

--- components/AccountBalance.vue
+++ components/AccountBalance.vue
@@ -1,18 +1,16 @@
 <template>
-     <div class="container" v-if="user">
-        <b-row class="text-right d-flex justify-content-between w-100">
-            <p>Steem Balance :</p> {{user.account.balance}} 
-        </b-row>
-        <b-row class="text-right d-flex justify-content-between w-100">
-            <p>SBD Balance :</p> {{user.account.sbd_balance}} 
-        </b-row>
-        <b-btn class="float-right" v-b-modal.transferModal>Transfer</b-btn>
-     </div>
+  <div class="container" v-if="user">
+    <b-row class="text-right d-flex justify-content-between w-100">
+      <p>Steem Balance:</p> {{user.account.balance}}
+    </b-row>
+    <b-row class="text-right d-flex justify-content-between w-100">
+      <p>SBD Balance:</p> {{user.account.sbd_balance}}
+    </b-row>
+    <b-btn class="float-right" v-b-modal.transferModal>Transfer</b-btn>
+  </div>
 </template>
 <script>
   export default {
-    props: ['user'],
-    computed: {
-    },
+    props: ['user']
   }
 </script>
