932bdca200565167f75c9fefa7fb31066ac41061
mktcode <kontakt@markus-kottlaender.de>
2018-06-10 13:41:38 +0200

    updated footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="footer">
-    Made by <a href="https://steemit.com/@mkt" target="_blank">@mkt</a>
+    Made with <svg style="width:18px;height:18px;margin-bottom: -3px;" viewBox="0 0 24 24">
+      <path fill="#eebbcc" d="M12,21.35L10.55,20.03C5.4,15.36 2,12.27 2,8.5C2,5.41 4.42,3 7.5,3C9.24,3 10.91,3.81 12,5.08C13.09,3.81 14.76,3 16.5,3C19.58,3 22,5.41 22,8.5C22,12.27 18.6,15.36 13.45,20.03L12,21.35Z" />
+    </svg> by <a href="https://steemit.com/@mkt" target="_blank">@mkt</a><br>
+    <br>
+    <b>Maintainers:</b><br>
+    French: <a href="https://steemit.com/@helo">@helo</a> (<a href="https://v2.steemconnect.com/sign/account-witness-vote?witness=helo&approve=1">Vote for Witness!</a>)<br>
+    German: <a href="https://steemit.com/@mkt">@mkt</a>
   </div>
 </template>
 
 <style lang="sass">
   #footer
     text-align: center
-    color: #ddd
+    color: #ccc
     font-size: 1rem
     padding: 25px
 
   #footer a
-    color: #ddd
+    color: #ccc
 </style>
