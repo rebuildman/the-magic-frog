0e70b57eb038ebd06aa169a4b1bc795bbfd61d61
Gregory Luneau <gregory.luneau@gmail.com>
2018-06-12 12:41:01 -0400

    Update StoryPart.vue
    by and by, do not forget by.

--- components/StoryPart.vue
+++ components/StoryPart.vue
@@ -4,7 +4,7 @@
     <div v-if="part.image" class="mb-1">
       <img :src="part.image" alt="" class="img-fluid" />
     </div>
-    <sup>by <a :href="'https://steemit.com/@' + part.author" target="_blank">@{{ part.author }}</a></sup>
+    <sup>{{ $t('footer.by') }} <a :href="'https://steemit.com/@' + part.author" target="_blank">@{{ part.author }}</a></sup>
   </div>
 </template>
 
