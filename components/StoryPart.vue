55c369389e2752b40b642df4323b5c468b355884
mktcode <kontakt@markus-kottlaender.de>
2018-06-29 16:52:36 +0200

    show The End for last entry

--- components/StoryPart.vue
+++ components/StoryPart.vue
@@ -4,6 +4,7 @@
     <div v-if="part.image" class="mb-1">
       <img :src="part.image" alt="" class="img-fluid" />
     </div>
+    <h3 v-if="part.type === 'end'">{{ $t('index.form.theend') }}</h3>
     <sup>{{ $t('footer.by') }} <a :href="'https://steemit.com/@' + part.author" target="_blank">@{{ part.author }}</a></sup>
   </div>
 </template>
