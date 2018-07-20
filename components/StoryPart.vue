933e8c47c20815b4cd620fed9f0797358f6ae765
mktcode <kontakt@markus-kottlaender.de>
2018-07-21 00:58:57 +0200

    limited image width in story

--- components/StoryPart.vue
+++ components/StoryPart.vue
@@ -2,7 +2,7 @@
   <div class="text-center mb-3">
     <div v-if="part.appendText" v-html="appendHtml"></div>
     <div v-if="part.image" class="mb-1">
-      <img :src="part.image" alt="" class="img-fluid" />
+      <img :src="part.image" alt="" class="img-fluid" style="max-width: 100%; width: 800px;" />
     </div>
     <h3 v-if="part.type === 'end'">{{ $t('index.form.theend') }}</h3>
     <sup>{{ $t('footer.by') }} <a :href="'https://steemit.com/@' + part.author" target="_blank">@{{ part.author }}</a></sup>
