94b1335ab2cc0b0f833947b5ea874b64183a957c
Gregory Luneau <gregory.luneau@gmail.com>
2018-08-14 08:40:48 -0400

    more linting

--- plugins/i18n.js
+++ plugins/i18n.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import VueI18n from 'vue-i18n'
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 
-Vue.use(VueI18n)
+Vue.use(VueI18n);
 
 export default ({ app, store }, inject) => {
   let locale = process.env.locale;
@@ -9,31 +9,31 @@ export default ({ app, store }, inject) => {
     locale = 'en';
   }
   app.i18n = new VueI18n({
-    locale: locale,
+    locale,
     fallbackLocale: 'en',
     messages: {
-      'en': require('~/locales/en.json'),
-      'de': require('~/locales/de.json'),
-      'fr': require('~/locales/fr.json'),
-      'pt': require('~/locales/pt.json'),
-    }
-  })
+      en: require('~/locales/en.json'),
+      de: require('~/locales/de.json'),
+      fr: require('~/locales/fr.json'),
+      pt: require('~/locales/pt.json'),
+    },
+  });
 
   app.i18n.path = (link) => {
     if (app.i18n.locale === app.i18n.fallbackLocale) {
-      return `/${link}`
+      return `/${link}`;
     }
 
-    return `/${app.i18n.locale}/${link}`
-  }
+    return `/${app.i18n.locale}/${link}`;
+  };
 
-  let accounts = {
+  const accounts = {
     en: 'the-magic-frog',
     de: 'der-zauberfrosch',
     fr: 'grenouille',
     pt: 'sapo-magico',
-  }
+  };
 
-  app.account = accounts[app.i18n.locale]
-  Vue.prototype.$account = app.account
-}
+  app.account = accounts[app.i18n.locale];
+  Vue.prototype.$account = app.account;
+};
